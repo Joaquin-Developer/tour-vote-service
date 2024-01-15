9164f6a38f16cf7e91d2fc5a34a0b8b0da52440c
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2024-01-15 20:41:53 -0300

    endpoint add_song

--- app/routes/songs.py
+++ app/routes/songs.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.db.database import get_session
 from app.models.models import Song
+from app.schemas.schemas import Song as SongSchema
 
 router = APIRouter()
 
@@ -18,3 +19,20 @@ def get_all_songs():
         dict(zip(row._mapping.keys(), row))
         for row in results
     ]
+
+
+@router.post("/add_song")
+def add_song(name: str, album: str = None, description: str = None):
+    """
+    Add a new song to the database
+    """
+    with get_session() as session:
+        new_song = Song(
+            name=name,
+            album=album,
+            description=description
+        )
+
+        session.add(new_song)
+        session.commit()
+    return {"message": "Song added successfully"}
